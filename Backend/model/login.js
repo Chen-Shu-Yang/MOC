e88a791ec19e6caeeb50c01680c133392f153413
Farhan <MNURFARHAN13.20@ichat.sp.edu.sg>
2022-06-01 00:10:10 +0800

    updatedLogin

--- Backend/model/login.js
+++ Backend/model/login.js
@@ -22,7 +22,6 @@ const Login = {
     FROM heroku_6b49aedb7855c0b.admin 
     Where Email = ? and Password = ?`;
     // pool query
-    console.log('Bih');
     pool.query(sql, [email, password], (err, result) => {
       // error
       // if (verified) {
@@ -30,29 +29,103 @@ const Login = {
         console.log(err);
         return callback(err, null);
       } if (result.length === 0) {
-        return callback(null, null);
-      }
-      // there must only be 1 result here
+        // sql query statement
+        const sql = `SELECT *
+    FROM heroku_6b49aedb7855c0b.customer 
+    Where Email = ? and Password = ?`;
+        // pool query
+        pool.query(sql, [email, password], (err, result) => {
+          // error
+          // if (verified) {
+          if (err) {
+            console.log(err);
+            return callback(err, null);
+          } if (result.length === 0) {
+            console.log('Bih');
+
+            // sql query statement
+            const sql = `SELECT *
+    FROM heroku_6b49aedb7855c0b.superadmin 
+    Where Email = ? and Password = ?`;
+            // pool query
+            pool.query(sql, [email, password], (err, result) => {
+              // error
+              // if (verified) {
+              if (err) {
+                console.log(err);
+                return callback(err, null);
+              } if (result.length === 0) {
+                console.log('Bih');
+              }
+              // there must only be 1 result here
+              // since email is unique
+              // confirm if we have the key
+              console.log(`secret config key${config.key}`);
+              console.log(result[0]);
+
+              // generate the token
+              const token = jwt.sign(
+                {
+                  // (1)Payload
+                  SuperAdminID: result[0].SuperAdminID,
+                },
+                // (2) Secret Key
+                config.key,
+                // (3) Lifetime of a token
+                {
+                  // expires in 24 hrs
+                  expiresIn: 86400,
+                },
+              );
+              return callback(null, token, result[0]);
+            });
+          } else {
+            // there must only be 1 result here
+          // since email is unique
+          // confirm if we have the key
+            console.log(`secret config key${config.key}`);
+            console.log(result[0]);
+
+            // generate the token
+            const token = jwt.sign(
+              {
+              // (1)Payload
+                CustomerID: result[0].CustomerID,
+              },
+              // (2) Secret Key
+              config.key,
+              // (3) Lifetime of a token
+              {
+              // expires in 24 hrs
+                expiresIn: 86400,
+              },
+            );
+            return callback(null, token, result[0]);
+          }
+        });
+      } else {
+        // there must only be 1 result here
       // since email is unique
       // confirm if we have the key
-      console.log(`secret config key${config.key}`);
-      console.log(result[0]);
+        console.log(`secret config key${config.key}`);
+        console.log(result[0]);
 
-      // generate the token
-      const token = jwt.sign(
-        {
+        // generate the token
+        const token = jwt.sign(
+          {
           // (1)Payload
-          UserId: result[0].UserId,
-        },
-        // (2) Secret Key
-        config.key,
-        // (3) Lifetime of a token
-        {
+            UserId: result[0].UserId,
+          },
+          // (2) Secret Key
+          config.key,
+          // (3) Lifetime of a token
+          {
           // expires in 24 hrs
-          expiresIn: 86400,
-        },
-      );
-      return callback(null, token, result[0]);
+            expiresIn: 86400,
+          },
+        );
+        return callback(null, token, result[0]);
+      }
     });
   },
 
