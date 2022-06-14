b0d9b3df15ccb470523a2d97bcb7aa1efc343256
rameshMonika <Monika.20@ichat.sp.edu.sg>
2022-06-15 02:14:20 +0800

    all backend and simple frontend done

--- Frontend/public/assets/js/adminDashboard.js
+++ Frontend/public/assets/js/adminDashboard.js
@@ -12,45 +12,45 @@ function loadMonthlyBookingForGraph() {
         contentType: "application/json; charset=utf-8",
         success: function (data, textStatus, xhr) {
 
-            var numberOfBookingJan,numberOfBookingFeb,numberOfBookingMar,numberOfBookingApr,numberOfBookingMay,numberOfBookingJun,numberOfBookingJul,numberOfBookingAug,numberOfBookingSep,numberOfBookingOct,numberOfBookingNov,numberOfBookingDec;
-            numberOfBookingJan=data[0].numberOfBooking
-            numberOfBookingFeb=data[1].numberOfBooking
-            numberOfBookingMar=data[2].numberOfBooking
-            numberOfBookingApr=data[3].numberOfBooking
-            numberOfBookingMay=data[4].numberOfBooking
-            numberOfBookingJun=data[5].numberOfBooking
-            numberOfBookingJul=data[6].numberOfBooking
-            numberOfBookingAug=data[7].numberOfBooking
-            numberOfBookingSep=data[8].numberOfBooking
-            numberOfBookingOct=data[9].numberOfBooking
-            numberOfBookingNov=data[10].numberOfBooking
-            numberOfBookingDec=data[11].numberOfBooking
-          
-          
+            var numberOfBookingJan, numberOfBookingFeb, numberOfBookingMar, numberOfBookingApr, numberOfBookingMay, numberOfBookingJun, numberOfBookingJul, numberOfBookingAug, numberOfBookingSep, numberOfBookingOct, numberOfBookingNov, numberOfBookingDec;
+            numberOfBookingJan = data[0].numberOfBooking
+            numberOfBookingFeb = data[1].numberOfBooking
+            numberOfBookingMar = data[2].numberOfBooking
+            numberOfBookingApr = data[3].numberOfBooking
+            numberOfBookingMay = data[4].numberOfBooking
+            numberOfBookingJun = data[5].numberOfBooking
+            numberOfBookingJul = data[6].numberOfBooking
+            numberOfBookingAug = data[7].numberOfBooking
+            numberOfBookingSep = data[8].numberOfBooking
+            numberOfBookingOct = data[9].numberOfBooking
+            numberOfBookingNov = data[10].numberOfBooking
+            numberOfBookingDec = data[11].numberOfBooking
 
-            var xValues = ["January","February","March","April","May","June","July","August","October","Novemeber","December"];
-            var yValues = [numberOfBookingJan,numberOfBookingFeb,numberOfBookingMar,numberOfBookingApr,numberOfBookingMay,numberOfBookingJun,numberOfBookingJul,numberOfBookingAug,numberOfBookingSep,numberOfBookingOct,numberOfBookingNov,numberOfBookingDec];
-            
-            colorVal="rgba(242, 70, 141, 0.8)"
-            
+
+
+            var xValues = ["January", "February", "March", "April", "May", "June", "July", "August", "October", "Novemeber", "December"];
+            var yValues = [numberOfBookingJan, numberOfBookingFeb, numberOfBookingMar, numberOfBookingApr, numberOfBookingMay, numberOfBookingJun, numberOfBookingJul, numberOfBookingAug, numberOfBookingSep, numberOfBookingOct, numberOfBookingNov, numberOfBookingDec];
+
+            colorVal = "rgba(242, 70, 141, 0.8)"
+
             new Chart("myChart", {
-              type: "line",
-              data: {
-                labels: xValues,
-                datasets: [{
-                  fill: true,
-                  lineTension: 0.2,
-                  backgroundColor:colorVal ,
-                  strokeColor: "rgba(0,0,0,0.8)",
-                  data: yValues
-                }]
-              },
-              options: {
-                legend: {display: false},
-                scales: {
-                  yAxes: [{ticks: {min: 0,autoSkip: true}}],
+                type: "line",
+                data: {
+                    labels: xValues,
+                    datasets: [{
+                        fill: true,
+                        lineTension: 0.2,
+                        backgroundColor: colorVal,
+                        strokeColor: "rgba(0,0,0,0.8)",
+                        data: yValues
+                    }]
+                },
+                options: {
+                    legend: { display: false },
+                    scales: {
+                        yAxes: [{ ticks: { min: 0, autoSkip: true } }],
+                    }
                 }
-              }
             });
 
         },
@@ -79,9 +79,67 @@ function getRevenueOfTheMonth() {
             console.log(data)
 
             $('#revenueOfTheMonth').append(data.totalRevenue);
+
+
+
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            //print error
+            console.log('Error in Operation');
+        }
+    });
+
+
+
+
+
+}
+
+function diffInConsecutiveMonthBooking() {
+    $.ajax({
+        url: `${backEndUrl}/bookingsByMonth`,
+        type: 'GET',
+        contentType: "application/json; charset=utf-8",
+        success: function (data, textStatus, xhr) {
+            const currentDate = new Date();
+            let currentMonth = currentDate.getMonth();
+            let lastMonth;
+
+            if(currentMonth==0){
+                lastMonth=12
+            }
+            else{
+                lastMonth=currentMonth-1
+            }
+
           
 
-         
+            var numberOfBookingLastMonth,numberOfBookingCurrenttMonth;
+            numberOfBookingLastMonth = data[lastMonth].numberOfBooking
+            numberOfBookingCurrenttMonth = data[currentMonth].numberOfBooking
+
+            var diffInBooking=numberOfBookingCurrenttMonth-numberOfBookingLastMonth
+
+           
+
+            console.log("Difference in booking: "+diffInBooking)
+
+            if(diffInBooking>0){
+                $('#diffInBooking').append("Positive: "+diffInBooking)
+
+
+            }
+            else if(diffInBooking ==0){
+                $('#diffInBooking').append("neutral: "+diffInBooking)
+            }
+            else{
+                $('#diffInBooking').append("negative: "+Math.abs(diffInBooking))
+
+            }
+        
+
+
+
         },
         error: function (xhr, textStatus, errorThrown) {
             //print error
@@ -96,6 +154,7 @@ function getRevenueOfTheMonth() {
 }
 
 
+
 // to load datas when page refresh or loads for the first time
 $(document).ready(function () {
     // to debug
@@ -105,8 +164,10 @@ $(document).ready(function () {
     console.log("Query Param (extraction): " + queryParams)
     //load
     loadMonthlyBookingForGraph();
-  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     getRevenueOfTheMonth()
-   
-   
+    console.log("+++++++++++++++++++++++++++++++++")
+    diffInConsecutiveMonthBooking()
+
+
 });
