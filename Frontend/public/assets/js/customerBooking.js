6b43430eaec3e9fa2dd809e94aebdd85318829ea
Farhan <MNURFARHAN13.20@ichat.sp.edu.sg>
2022-06-22 23:23:29 +0800

    button

--- Frontend/public/assets/js/customerBooking.js
+++ Frontend/public/assets/js/customerBooking.js
@@ -22,8 +22,8 @@ function createCard(cardInfo) {
         <div class="card">
 
             <div id="service${cardInfo.ClassID}" class="container-class"  border-radius: 10px;">
-                <h4><b>${cardInfo.ClassName}</b></h4> 
-                <p>$${cardInfo.ClassPricing} per hour</p>
+                <h4 style="text-align:center;"><b>${cardInfo.ClassName}</b></h4> 
+                <p style="text-align:center;">$${cardInfo.ClassPricing} per hour</p>
                 <p>Include:</p>
                 <p>${cardInfo.ClassDes}</p>
              
@@ -384,6 +384,140 @@ function updatedTime() {
 }
 
 $(document).ready(() => {
+  $('.btn-numberRR').click(function (e) {
+    e.preventDefault();
+
+    fieldName = $(this).attr('data-field');
+    type = $(this).attr('data-type');
+    const input = $(`input[name='${fieldName}']`);
+    const currentVal = parseInt(input.val(), 10);
+    // eslint-disable-next-line no-restricted-globals
+    if (!isNaN(currentVal)) {
+      if (type === 'minus') {
+        if (currentVal > input.attr('min')) {
+          input.val(currentVal - 1).change();
+        }
+        if (parseInt(input.val(), 10) === input.attr('min')) {
+          $(this).attr('disabled', true);
+        }
+      } else if (type === 'plus') {
+        if (currentVal < input.attr('max')) {
+          input.val(currentVal + 1).change();  
+        }
+        if (parseInt(input.val(), 10) === input.attr('max')) {
+          $(this).attr('disabled', true);
+        }
+      }
+    } else {
+      input.val(0);
+    }
+  });
+  $('.input-numberRR').focusin(function () {
+    $(this).data('oldValue', $(this).val());
+  });
+  $('.input-numberRR').change(function () {
+    minValue = parseInt($(this).attr('min'), 10);
+    maxValue = parseInt($(this).attr('max'), 10);
+    valueCurrent = parseInt($(this).val(), 10);
+
+    // eslint-disable-next-line no-restricted-globals
+    // eslint-disable-next-line no-global-assign
+    names = $(this).attr('name');
+    if (valueCurrent >= minValue) {
+      $(`.btn-number[data-type='minus'][data-field='${names}']`).removeAttr('disabled');
+    } else {
+      alert('Sorry, the minimum value was reached');
+      $(this).val($(this).data('oldValue'));
+    }
+    if (valueCurrent <= maxValue) {
+      $(`.btn-number[data-type='plus'][data-field='${names}']`).removeAttr('disabled');
+    } else {
+      alert('Sorry, the maximum value was reached');
+      $(this).val($(this).data('oldValue'));
+    }
+  });
+  $('.input-numberBR').keydown((e) => {
+    // Allow: backspace, delete, tab, escape, enter and .
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1
+    // Allow: Ctrl+A
+            || (e.keyCode === 65 && e.ctrlKey === true)
+    // Allow: home, end, left, right
+            || (e.keyCode >= 35 && e.keyCode <= 39)) {
+      // let it happen, don't do anything
+      return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+      e.preventDefault();
+    }
+  });
+  $('.btn-numberBR').click(function (e) {
+    e.preventDefault();
+
+    fieldName = $(this).attr('data-field');
+    type = $(this).attr('data-type');
+    const input = $(`input[name='${fieldName}']`);
+    const currentVal = parseInt(input.val(), 10);
+    // eslint-disable-next-line no-restricted-globals
+    if (!isNaN(currentVal)) {
+      if (type === 'minus') {
+        if (currentVal > input.attr('min')) {
+          input.val(currentVal - 1).change();
+        }
+        if (parseInt(input.val(), 10) === input.attr('min')) {
+          $(this).attr('disabled', true);
+        }
+      } else if (type === 'plus') {
+        if (currentVal < input.attr('max')) {
+          input.val(currentVal + 1).change();  
+        }
+        if (parseInt(input.val(), 10) === input.attr('max')) {
+          $(this).attr('disabled', true);
+        }
+      }
+    } else {
+      input.val(0);
+    }
+  });
+  $('.input-numberBR').focusin(function () {
+    $(this).data('oldValue', $(this).val());
+  });
+  $('.input-numberBR').change(function () {
+    minValue = parseInt($(this).attr('min'), 10);
+    maxValue = parseInt($(this).attr('max'), 10);
+    valueCurrent = parseInt($(this).val(), 10);
+
+    // eslint-disable-next-line no-restricted-globals
+    // eslint-disable-next-line no-global-assign
+    names = $(this).attr('name');
+    if (valueCurrent >= minValue) {
+      $(`.btn-number[data-type='minus'][data-field='${names}']`).removeAttr('disabled');
+    } else {
+      alert('Sorry, the minimum value was reached');
+      $(this).val($(this).data('oldValue'));
+    }
+    if (valueCurrent <= maxValue) {
+      $(`.btn-number[data-type='plus'][data-field='${names}']`).removeAttr('disabled');
+    } else {
+      alert('Sorry, the maximum value was reached');
+      $(this).val($(this).data('oldValue'));
+    }
+  });
+  $('.input-numberBR').keydown((e) => {
+    // Allow: backspace, delete, tab, escape, enter and .
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1
+    // Allow: Ctrl+A
+            || (e.keyCode === 65 && e.ctrlKey === true)
+    // Allow: home, end, left, right
+            || (e.keyCode >= 35 && e.keyCode <= 39)) {
+      // let it happen, don't do anything
+      return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+      e.preventDefault();
+    }
+  });
   loadUserDetails();
   populateClass();
   populatePackage();
