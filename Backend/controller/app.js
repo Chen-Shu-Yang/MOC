2218ae450d7f237183cb6ff1f48e23e3b6c8cd89
Chen-shuyang <99723175+Chen-Shu-Yang@users.noreply.github.com>
2022-07-13 00:32:33 +0800

    Update app.js

--- Backend/controller/app.js
+++ Backend/controller/app.js
@@ -223,7 +223,7 @@ const sendVerificationEmail = ({ _id, email }, res) => {
   };
 
   // hash the uniqueString
-  const saltRounds = process.env.SALT_ROUNDS;
+  const saltRounds = 10;
   bcrypt
     .hash(UniqueString, saltRounds)
     .then((hashedUniqueString) => {
