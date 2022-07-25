ff4ba28ead574a629735e74fc329afb0f9eaf33c
Farhan <MNURFARHAN13.20@ichat.sp.edu.sg>
2022-07-25 22:32:04 +0800

    Update customerBooking.js

--- Frontend/public/assets/js/customerBooking.js
+++ Frontend/public/assets/js/customerBooking.js
@@ -5,12 +5,13 @@
 /* eslint-disable prefer-regex-literals */
 /* eslint-disable no-useless-escape */
 
-const frontEndUrl = 'http://localhost:3001';
-const backEndUrl = 'http://localhost:5000';
-// const frontEndUrl = 'https://moc-fa.herokuapp.com';
-// const backEndUrl = 'https://moc-ba.herokuapp.com';
-// const frontEndUrl = 'http://18.142.170.203:3001';
-// const backEndUrl = 'http://18.142.170.203:5000';
+// const frontEndUrl = 'http://localhost:3001';
+// const backEndUrl = 'http://localhost:5000';
+const frontEndUrl = 'https://moc-fa.herokuapp.com';
+const backEndUrl = 'https://moc-ba.herokuapp.com';
+// const frontEndUrl = 'http://18.142.170.203:3001/';
+// const backEndUrl = 'http://18.142.170.203:5000/';
+
 const tmpToken = JSON.parse(localStorage.getItem('token'));
 if (tmpToken === null) {
   window.location.replace(`${frontEndUrl}/unAuthorize`);
@@ -688,7 +689,7 @@ $(document).ready(() => {
         type: 'error',
         layout: 'topCenter',
         theme: 'sunset',
-        text: 'Please ensure the Day is different',
+        text: 'Please ensure the 2 days of service is different',
       }).show();
 
     }else {
